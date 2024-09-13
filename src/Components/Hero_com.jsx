ec9fe637840280207d440e2e90bc70cc7c4df48e
prajwalpal28 <palprajwal18@gmail.com>
2024-09-13 22:36:42 +0530

    Project Complete

--- src/Components/Hero_com.jsx
+++ src/Components/Hero_com.jsx
@@ -4,9 +4,10 @@ import { Link } from 'react-router-dom';
 
 export const Hero_com = () => {
     return (
-        <div className="flex flex-col md:flex-row items-center justify-center min-h-screen mx-auto py-16 px-4 md:px-8 lg:px-16 xl:px-24 pt-28 md:pt-40">
+        <div className="flex flex-col md:flex-row flex-wrap items-stretch justify-center min-h-screen mx-auto py-40 px-4 md:px-8 lg:px-16 xl:px-24 pt-28 md:pt-40">
 
-            <div className="flex flex-col gap-6 bg-black bg-opacity-40 text-white py-10 px-6 sm:px-8 md:px-10 rounded-lg shadow-lg border border-opacity-15 border-gray max-w-full md:max-w-2xl">
+            {/* Left Section */}
+            <div className="flex flex-col justify-center gap-6 bg-black bg-opacity-40 text-white py-10 px-6 sm:px-8 md:px-10 rounded-lg shadow-lg border border-opacity-15 border-gray max-w-full md:max-w-2xl">
 
                 <div>
                     <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold text-white mb-4 md:mb-6">
@@ -19,23 +20,24 @@ export const Hero_com = () => {
                     </p>
                 </div>
 
-                <div className="flex flex-wrap space-x-4 sm:space-x-6 md:space-x-8">
+                <div className="flex flex-wrap gap-4 sm:gap-6 md:gap-8">
                     <button className="border-2 border-gray-800 rounded-lg px-3 py-2 text-gray cursor-pointer hover:text-gray-200 hover:bg-indigo-500 hover:text-white transition duration-300 hover:scale-110 transform shadow-[0px_0px_55px_2px_#1a202c]">
-                        <a href="https://docs.google.com/document/d/1UMP-q0cg3g-6ioganaBhF0Dsoa0ByrqWWEaNSGZiX9s/edit?usp=drive_link" target='_blank' rel='noopener noreferrer'>
+                        <a href="https://docs.google.com/document/d/1UMP-q0cg3g-6ioganaB" target='_blank' rel='noopener noreferrer'>
                             See my resume
                         </a>
                     </button>
                     <Link to='/contact'>
                         <button className="bg-transparent border-2 border-gray text-indigo-500 py-2 px-3 sm:px-4 md:px-6 rounded-lg hover:bg-indigo-500 hover:text-white transition duration-300 hover:scale-110 transform shadow-[0px_0px_55px_2px_#1a202c]">
-                            <a href="">Get in touch</a>
+                            Get in touch
                         </button>
                     </Link>
                 </div>
             </div>
 
-            <div className="mt-12 md:mt-0 md:ml-16 flex-shrink-0 shadow-[0px_0px_55px_10px_#1a202c]">
-                <div className="bg-black bg-opacity-40 p-6 rounded-lg shadow-lg border border-opacity-15 border-gray">
-                    <img src={Hero_imgg} alt="Prajwal's Photo" className="w-56 h-56 sm:w-64 sm:h-64 md:w-96 md:h-96 object-cover" />
+            {/* Right Section (Image) */}
+            <div className="mt-12 md:mt-0 md:ml-16 flex-shrink-0 flex items-center justify-center shadow-[0px_0px_55px_10px_#1a202c] w-full md:w-auto">
+                <div className="bg-black bg-opacity-40 p-6 rounded-lg shadow-lg border border-opacity-15 border-gray h-full">
+                    <img src={Hero_imgg} alt="Prajwal's Photo" className="w-48 h-48 sm:w-56 sm:h-56 md:w-96 md:h-96 object-cover" />
                 </div>
             </div>
         </div>
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,66 +3,143 @@ import { Link } from 'react-router-dom';
 import github_logo from '../assets/github_logo.png';
 import twitter_logo from '../assets/twitter_logo.png';
 import linkedin_logo from '../assets/linkedin_logo.png';
+import { FiMenu, FiX } from 'react-icons/fi'; // Using react-icons for the toggle button
 
 export const Navbar = () => {
     const [menu, setMenu] = useState("Prajwal.Pal");
+    const [isOpen, setIsOpen] = useState(false); // State for toggling the menu on small screens
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
 
     return (
-        <div className="fixed top-0 left-0 w-full z-50">
-            <div className='flex justify-center gap-12'>
-                <ul className='flex justify-evenly gap-12 mt-10 bg-navblur bg-opacity-30 text-white p-4 rounded-lg shadow-lg border border-border backdrop-blur-3xl'>
+        <div className="md:flex md:flex-row md:justify-center fixed top-0 left-0 w-full z-50">
+
+            {/* Main Navbar Container */}
+            <div className='flex justify-center md:justify-between gap-12 items-center px-4 md:px-12 py-4 bg-opacity-30 text-white '>
+
+                {/* Left Section - Name/Logo */}
+                <div className='md:flex justify-center gap-12 hidden'>
+                    <ul className='flex justify-evenly gap-12 mt-10 bg-navblur bg-opacity-30 text-white p-4 rounded-lg shadow-lg border border-border backdrop-blur-3xl'>
+                        <li
+                            onClick={() => setMenu("Prajwal.Pal")}
+                            className={`hover:scale-110 transform transition-transform duration-300 hover:cursor-pointer ${menu === "Prajwal.Pal" ? "font-bold" : ""}`}
+                        >
+                            <Link to='/'>Prajwal.Pal</Link>
+                        </li>
+                        <li
+                            onClick={() => setMenu("About")}
+                            className={`hover:scale-110 transform transition-transform duration-300 hover:cursor-pointer ${menu === "About" ? "font-bold" : ""}`}
+                        >
+                            <Link to='/about'>About</Link>
+                        </li>
+                        <li
+                            onClick={() => setMenu("Projects")}
+                            className={`hover:scale-110 transform transition-transform duration-300 hover:cursor-pointer ${menu === "Projects" ? "font-bold" : ""}`}
+                        >
+                            <Link to='/projects'>Projects</Link>
+                        </li>
+                        <li
+                            onClick={() => setMenu("Techstack")}
+                            className={`hover:scale-110 transform transition-transform duration-300 hover:cursor-pointer ${menu === "Techstack" ? "font-bold" : ""}`}
+                        >
+                            <Link to='/techstack'>Techstack</Link>
+                        </li>
+                        <li
+                            onClick={() => setMenu("Contact")}
+                            className={`hover:scale-110 transform transition-transform duration-300 hover:cursor-pointer ${menu === "Contact" ? "font-bold" : ""}`}
+                        >
+                            <Link to='/contact'>Contact</Link>
+                        </li>
+                    </ul>
+                </div>
+
+                {/* Toggle Button for Mobile Devices */}
+                <div className="flex gap-44 items-center md:hidden rounded-lg shadow-lg bg-navblur bg-opacity-30 text-white border border-border p-4 backdrop-blur-3xl">
                     <li
-                        onClick={() => setMenu("Prajwal.Pal")}
-                        className={`hover:scale-110 transform transition-transform duration-300 hover:cursor-pointer ${menu === "Prajwal.Pal" ? "font-bold" : ""}`}
+                        onClick={() => { setMenu("Prajwal.Pal"); setIsOpen(false); }}
+                        className={`hover:scale-110 text-2xl list-none transform transition-transform duration-300 ${menu === "Prajwal.Pal" ? "font-bold" : ""}`}
                     >
                         <Link to='/'>Prajwal.Pal</Link>
                     </li>
-                    <li
-                        onClick={() => setMenu("About")}
-                        className={`hover:scale-110 transform transition-transform duration-300 hover:cursor-pointer ${menu === "About" ? "font-bold" : ""}`}
-                    >
-                        <Link to='/about'>About</Link>
-                    </li>
-                    <li
-                        onClick={() => setMenu("Projects")}
-                        className={`hover:scale-110 transform transition-transform duration-300 hover:cursor-pointer ${menu === "Projects" ? "font-bold" : ""}`}
-                    >
-                        <Link to='/projects'>Projects</Link>
-                    </li>
-                    <li
-                        onClick={() => setMenu("Techstack")}
-                        className={`hover:scale-110 transform transition-transform duration-300 hover:cursor-pointer ${menu === "Techstack" ? "font-bold" : ""}`}
-                    >
-                        <Link to='/techstack'>Techstack</Link>
-                    </li>
-                    <li
-                        onClick={() => setMenu("Contact")}
-                        className={`hover:scale-110 transform transition-transform duration-300 hover:cursor-pointer ${menu === "Contact" ? "font-bold" : ""}`}
-                    >
-                        <Link to='/contact'>Contact</Link>
-                    </li>
-                </ul>
+                    <button onClick={toggleMenu} className="text-white focus:outline-none">
+                        {isOpen ? <FiX className="h-10 w-10" /> : <FiMenu className="h-10 w-10" />}
+                    </button>
+                </div>
+            </div>
 
-                <ul className='flex justify-evenly gap-4 mt-10 bg-black bg-opacity-30 text-white p-4 rounded-lg border border-border backdrop-blur-3xl'>
+            {/* Dropdown Menu for Mobile Devices */}
+            <ul className={`md:hidden flex flex-col items-center gap-6 p-4 rounded-lg shadow-lg border border-border backdrop-blur-3xl text-white absolute w-1/2 top-16 right-4 transition-all duration-300 ${isOpen ? 'block' : 'hidden'}`}>
+
+                <li
+                    onClick={() => { setMenu("About"); setIsOpen(false); }}
+                    className={`hover:scale-110 transform transition-transform duration-300 ${menu === "About" ? "font-bold" : ""}`}
+                >
+                    <Link to='/about'>About</Link>
+                </li>
+                <li
+                    onClick={() => { setMenu("Projects"); setIsOpen(false); }}
+                    className={`hover:scale-110 transform transition-transform duration-300 ${menu === "Projects" ? "font-bold" : ""}`}
+                >
+                    <Link to='/projects'>Projects</Link>
+                </li>
+                <li
+                    onClick={() => { setMenu("Techstack"); setIsOpen(false); }}
+                    className={`hover:scale-110 transform transition-transform duration-300 ${menu === "Techstack" ? "font-bold" : ""}`}
+                >
+                    <Link to='/techstack'>Techstack</Link>
+                </li>
+                <li
+                    onClick={() => { setMenu("Contact"); setIsOpen(false); }}
+                    className={`hover:scale-110 transform transition-transform duration-300 ${menu === "Contact" ? "font-bold" : ""}`}
+                >
+                    <Link to='/contact'>Contact</Link>
+                </li>
+
+                {/* Social Links for Mobile View */}
+                <ul className="flex justify-center gap-4 mt-4">
                     <li>
                         <a href="https://github.com/prajwalpal28" target="_blank" rel="noreferrer">
                             <img src={github_logo} alt="github" className="h-6 w-6 invert hover:scale-110 transform transition-transform duration-300 hover:cursor-pointer" />
                         </a>
                     </li>
-                    <h1 className='font-bold'>|</h1>
+                    <h1 className="font-bold">|</h1>
                     <li>
                         <a href="https://www.linkedin.com/in/prajwalpal28/" target="_blank" rel="noreferrer">
                             <img src={linkedin_logo} alt="linkedin" className="h-6 w-6 invert hover:scale-110 transform transition-transform duration-300 hover:cursor-pointer" />
                         </a>
                     </li>
-                    <h1 className='font-bold'>|</h1>
+                    <h1 className="font-bold">|</h1>
                     <li>
                         <a href="https://x.com/Prajwalpal18" target="_blank" rel="noreferrer">
                             <img src={twitter_logo} alt="twitter" className="h-6 w-6 invert hover:scale-110 transform transition-transform duration-300 hover:cursor-pointer" />
                         </a>
                     </li>
                 </ul>
-            </div>
+            </ul>
+
+            {/* Social Icons for Desktop View */}
+            <ul className="hidden md:flex justify-evenly gap-4 mt-14 mb-4 bg-black bg-opacity-30 text-white p-4 rounded-lg border border-border backdrop-blur-3xl">
+                <li>
+                    <a href="https://github.com/prajwalpal28" target="_blank" rel="noreferrer">
+                        <img src={github_logo} alt="github" className="h-6 w-6 invert hover:scale-110 transform transition-transform duration-300 hover:cursor-pointer" />
+                    </a>
+                </li>
+                <h1 className="font-bold">|</h1>
+                <li>
+                    <a href="https://www.linkedin.com/in/prajwalpal28/" target="_blank" rel="noreferrer">
+                        <img src={linkedin_logo} alt="linkedin" className="h-6 w-6 invert hover:scale-110 transform transition-transform duration-300 hover:cursor-pointer" />
+                    </a>
+                </li>
+                <h1 className="font-bold">|</h1>
+                <li>
+                    <a href="https://x.com/Prajwalpal18" target="_blank" rel="noreferrer">
+                        <img src={twitter_logo} alt="twitter" className="h-6 w-6 invert hover:scale-110 transform transition-transform duration-300 hover:cursor-pointer" />
+                    </a>
+                </li>
+            </ul>
+
         </div>
     );
-}
+};
